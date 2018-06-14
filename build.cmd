3851fe3405e00b811a979eaaf4fa51fcb31b9057
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-14 16:22:04 +0100

    Fix release notes link

--- build.cmd
+++ build.cmd
@@ -3,10 +3,13 @@
 SET VERSION=0.0.0
 IF NOT [%1]==[] (set VERSION=%1)
 
+SET TAG=0.0.0
+IF NOT [%2]==[] (set TAG=%2)
+
 curl -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 .\nuget.exe restore .\src\ViewModels.RavenDB.Tests\ViewModels.RavenDB.Tests.csproj -PackagesDirectory .\src\packages -Verbosity detailed
 
 dotnet test src/ViewModels.RavenDB.Tests/ViewModels.RavenDB.Tests.csproj -- RunConfiguration.TargetPlatform=x64 for RavenDB Tests
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack src/ViewModels.RavenDB/ViewModels.RavenDB.csproj -o ../../dist -p:Version=%version% -p:PackageVersion=%version% -c Release
+dotnet pack src/ViewModels.RavenDB/ViewModels.RavenDB.csproj -o ../../dist -p:Version=%version% -p:PackageVersion=%version% -p:Tag="%TAG%" -c Release
