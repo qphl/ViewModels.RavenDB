e553c6581fabc48bd9a09e254ac9268142c4522c
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-14 12:09:52 +0100

    Fixed build scripts

--- build.cmd
+++ build.cmd
@@ -3,6 +3,9 @@
 SET VERSION=0.0.0
 IF NOT [%1]==[] (set VERSION=%1)
 
+curl -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+.\nuget.exe restore .\src\ViewModels.RavenDB.Tests\ViewModels.RavenDB.Tests.csproj -PackagesDirectory .\src\packages -Verbosity detailed
+
 dotnet test src/ViewModels.RavenDB.Tests/ViewModels.RavenDB.Tests.csproj -- RunConfiguration.TargetPlatform=x64 for RavenDB Tests
 if %errorlevel% neq 0 exit /b %errorlevel%
 
--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ version="0.0.0"
 if [ -n "$1" ]; then version="$1"
 fi
 
-dotnet test src/ViewModels.RavenDB.Tests/ViewModels.RavenDB.Tests.csproj -- RunConfiguration.TargetPlatform=x64 for RavenDB Tests
+curl -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+.\nuget.exe restore .\src\ViewModels.RavenDB.Tests\ViewModels.RavenDB.Tests.csproj -PackagesDirectory .\src\packages -Verbosity detailed
 
-dotnet pack src/ViewModels.RavenDB/ViewModels.RavenDB.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
+dotnet test .\src\ViewModels.RavenDB.Tests\ViewModels.RavenDB.Tests.csproj -- RunConfiguration.TargetPlatform=x64 for RavenDB Tests
+
+dotnet pack .\src\ViewModels.RavenDB\ViewModels.RavenDB.csproj -o ..\..\dist -p:Version="$version" -p:PackageVersion="$version" -c Release
